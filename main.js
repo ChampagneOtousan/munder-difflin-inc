301d669ac9789e1702237fe3af8ceba0d8605b5f
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-14 14:12:51 -0400

    added slideshow function

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-document.body.style.backgroundImage = url("https://www.fdtgroup.org/wp-content/uploads/2016/05/Dalum-Papir.png")
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = "img1.jpg"
+images[1] = "img2.jpg"
+images[2] = "img3.jpg"
+images[3] = "img4.jpg"
+images[4] = "img5.jpg"
+
+function changeImg() {
+  document.slide.src = images[i];
+
+  if(i < images.lenght - 1){
+    i++;
+  } else {
+    i = 0;
+  }
+
+}
+
+setTimeOut("changeImg()", time)

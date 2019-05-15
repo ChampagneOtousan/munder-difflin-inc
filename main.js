39bbabc7e597dc0c6e67be3e4cba49e9d271a265
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-15 08:23:02 -0400

    fixed some shit

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-var i = 0;
-var images = [];
-var time = 3000;
+window.onscroll = function() {myFunction()};
 
-images[0] = "img1.jpg"
-images[1] = "img2.jpg"
-images[2] = "img3.jpg"
-images[3] = "img4.jpg"
-images[4] = "img5.jpg"
-
-function changeImg() {
-  document.slide.src = images[i];
-
-  if(i < images.lenght - 1){
-    i++;
+function myFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("para").className = "p";
   } else {
-    i = 0;
+    document.getElementById("img-grid").className = "img-grid";
   }
-
 }
 
-setTimeOut("changeImg()", time)
+// var i = 0;
+// var images = [];
+// var time = 3000;para
+//
+// images[0] = "img1.jpg"
+// images[1] = "img2.jpg"
+// images[2] = "img3.jpg"
+// images[3] = "img4.jpg"
+// images[4] = "img5.jpg"
+//
+// function changeImg() {
+//   document.slide.src = images[i];
+//
+//   if(i < images.lenght - 1){
+//     i++;
+//   } else {
+//     i = 0;
+//   }
+//
+// }
+//
+// setTimeOut("changeImg()", time)
